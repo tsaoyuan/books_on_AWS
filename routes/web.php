ea0e182fe9e8260ad44d47334d781ccee3a9fd47
tsao <tsaoyuan@gmail.com>
2023-04-28 13:57:55 +0800

    feat: 專案透過 Socialite 連接 GitHub OAuth
    - 專案內的第三方服務設定:
      - config\services.php: 在 Socialite 新增 drive: github
      - services.php 內容會有金鑰，所以放在 `.env` 比較安全
      - .env: 新增的格式和資訊如同 .env.example, 資訊是 GitHub OAuth 提供
      - 到 routes\web.php 設定路徑(路徑 /auth/redirect 參考 laravel 官方,路徑可以自行修改)
      - 畫面(views) 第三方登入的超連結 href 同 routes\web.php 的路徑設定 /auth/redircet
      - 畫面顯示 `GitHub OAuth successful` 表示 專案第三方服務設置成功, 有拿到 GitHub OAuth 提供的使用者資訊
    - `GitHub OAuth 設定` 要與 `專案內的第三方服務設定` 同步進行社定

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Laravel\Socialite\Facades\Socialite;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,3 +23,23 @@ Route::post('login', [\App\Http\Controllers\SessionsController::class, 'store'])
 Route::get('register', [\App\Http\Controllers\WebRegisterController::class, 'create']);
 Route::post('register', [\App\Http\Controllers\WebRegisterController::class, 'store']);
 Route::post('logout', [\App\Http\Controllers\SessionsController::class, 'destroy']);
+
+
+Route::get('/auth/redirect', function () {
+    // login 頁面點選通往Github OAtuh連結
+    // 導向 GitHub OAuth 登入頁面
+    // dd(Socialite::driver('github')->redirect());
+    return Socialite::driver('github')->redirect();
+});
+ 
+Route::get('/auth/callback', function () {
+
+    // dd(Socialite::driver('github')->user());
+    // 經過 GitHub OAuth 驗證後, GitHub OAuth 透過 /auth/callback 回傳使用者資訊給專案 
+    // 使用者的github資訊 設定為$user, 內含 name, email, token..等
+    $user = Socialite::driver('github')->user();
+    // 有拿到 GitHub OAuth 提供的使用者資訊, 表示專案第三方服務設置成功
+    return "GitHub OAuth successful"; 
+    // dd($user);
+    // $user->token
+});
